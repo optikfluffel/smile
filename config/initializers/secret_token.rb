e6ef9c9699de64bf6bc1cba91f03da661f540a8f
optikfluffel <optikfluffel@googlemail.com>
2012-10-22 13:17:54 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smile::Application.config.secret_token = '6ac82a197faacd3bf7e0fc8b9fd0549131d722f23e53be47618044bf88182a699070d959d93c2421e8f067b5896fe525f22a0af417507fe0754f2efe9a7ca5f9'
