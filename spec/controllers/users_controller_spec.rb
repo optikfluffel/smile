ceeb0de032ff1b300eb9f569b2b8b12e9b17e3a3
optikfluffel <optikfluffel@googlemail.com>
2012-10-23 18:57:49 +0200

    add rspec test for GET 'index'

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,7 +8,6 @@ describe UsersController do
   end
 
   describe "GET 'show'" do
-
     it "should be successful" do
       get :show, :id => @user.id
       response.should be_success
@@ -18,7 +17,13 @@ describe UsersController do
       get :show, :id => @user.id
       assigns(:user).should == @user
     end
+  end
 
+  describe "GET 'index'" do
+    it "should be successful" do
+      get 'index'
+      response.should be_success
+    end
   end
 
 end
