836eeb497cf66354fa1d4caed1ff3ffe886ef193
optikfluffel <optikfluffel@googlemail.com>
2012-11-15 20:31:52 +0100

    show only users you don't follow yet on find friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @users = User.find(:all, :conditions => ["id != ?", current_user.id])
+    @users = User.find(:all, :conditions => ["id not in (?)", current_user.followed_users.map(&:id).push(current_user.id)])
   end
 
   def show
