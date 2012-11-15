313e4f16296de46639d62fa56526c08c35e978d0
optikfluffel <optikfluffel@googlemail.com>
2012-11-15 15:53:57 +0100

    streamline users/index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @users = (current_user.blank? ? User.all : User.find(:all, :conditions => ["id != ?", current_user.id]))
+    @users = User.find(:all, :conditions => ["id != ?", current_user.id])
   end
 
   def show
