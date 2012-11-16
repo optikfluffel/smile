d6191484e8ff8a0b22e80ac0a2d80aa682c72311
optikfluffel <optikfluffel@googlemail.com>
2012-11-16 10:31:52 +0100

    add whitelist of filetypes to carrierwave configuration

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -27,5 +27,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
 end
